1ebffe56a624e4016b95cd9ba8752db3ecddf8ef
Anna <paestum1@gmail.com>
2016-09-09 19:23:42 +0200

    commit initial pour github pages du projet

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var soundTp = document.getElementById("sound");
+
+function animatecur(i) {
+  document.getElementsByClassName("listcontent")[i].style.height = "200px" ;
+  console.log("something");
+}
+function animateremove(){
+  document.getElementsByClassName("listcontent")[0].style.height = "0px" ;
+  document.getElementsByClassName("listcontent")[1].style.height = "0px" ;
+
+}
